65073cce990e669dc6ce888f461539445bf47575
Ama Williams <53152990+Tetsewa@users.noreply.github.com>
2024-02-08 21:31:19 +0100

    add iteration 1

--- js/game.js
+++ js/game.js
@@ -1,3 +1,39 @@
 class Game {
-    // code to be added
+ constructor (){
+    this.startScreen=document.getElementById("game-intro");
+    this.gameScreen=document.getElementById("game-screen");
+    this.gameEndScreen=document.getElementById("game-end");
+    this.player=null;
+    this.height=600;
+    this.width=500;
+    this.obstacles=[];
+    this.score=0;
+    this.lives=3;
+    this.gameIsOver=false;
+    this.gameIntervalId=gameIntervalId;
+    this.gameLoopFrecuency=1000/60
+ }   
+ start(){
+    this.gameScreen.style.height=`${this.height}px`;
+    this.gameScreen.style.width=`${this.width}px`;
+    this.startScreen.style.display="none";
+    this.gameScreen.style.displayy="block";
+    this.gameIntervalID= setInterval(()=>{
+        this.gameLoop()
+        
+    }, this.gameLoopFrecuency)
+       
+ }
+ gameLoop(){
+    console.log("in the game loop")
+    this.update();
+
+    if (this.gameIsOver){
+        clearInterval(this.gameIntervalId)
+    } 
+ }
+ update(){
+
+ }
 }
+new Game=start()
